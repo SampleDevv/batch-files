b23f5241db6ec3106568f85770959cad93fd8b14
Sample <sample.devy@gmail.com>
2021-02-02 23:52:26 -0500

    e

---
+++ fake-warning.bat
@@ -0,0 +1,6 @@
+@echo off
+msg * Windows AV | Virus Detected
+msg * Removal: Click "OK"
+msg * All Hardrives Formatted...
+msg * Please wait while Windows finishes virus cleanup...
+msg * lol (github.com/SampleDevv)
